f1955705d64dd9206e57dfbf1425d0360932573f
Elijah <Superil2q@yandex.ru>
2019-10-09 15:45:22 +0300

    project

---
+++ OutForm/Controls/SinPanel.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace OutForm.Controls
+{
+    partial class SinPanel
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.tbC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(6, 32);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(100, 20);
+            this.tbA.TabIndex = 0;
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(6, 71);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(100, 20);
+            this.tbB.TabIndex = 1;
+            // 
+            // tbC
+            // 
+            this.tbC.Location = new System.Drawing.Point(6, 110);
+            this.tbC.Name = "tbC";
+            this.tbC.Size = new System.Drawing.Size(100, 20);
+            this.tbC.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Амплитуда";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Частота";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Начальная фаза";
+            // 
+            // SinPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbC);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbA);
+            this.Name = "SinPanel";
+            this.Size = new System.Drawing.Size(115, 149);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox tbA;
+        public System.Windows.Forms.TextBox tbB;
+        public System.Windows.Forms.TextBox tbC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ SignalAnalysis/SignalAnalysis.h
@@ -0,0 +1,117 @@
+#define M_PI       3.14159265358979323846
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class AddNoise
+{
+public:
+
+	struct complex
+	{
+		double real = 0.0; double imagine = 0.0;
+	};
+
+	struct dot
+	{
+		complex amplitude; int x_pos;
+	};
+
+	int size = 0;
+
+	double noise_energy = 0;
+
+	double sign_energy = 0;
+
+	void quicksort(vector<dot> item, int len);
+
+	void qs(vector<dot> item, int left, int right);
+
+	vector <dot> signal;			//Массив структуры для кранения значений амплитуд
+
+	void SinGen(double ampl, double phase, double samp_freq, int first, int last);		//Функция генящая синусоиду
+
+	void SignalGen(int sin_count, double _ampl[], double _phase[], double _samp_freq[], int _first[], int _last[]);		//функция складывающая синусоиды
+
+	void SignalwithWhiteNoise(vector<dot> k, int percent);
+
+	void SinGen(double ampl, double phase, double samp_freq, int first, int last)
+	{
+
+		for (int i = first; i < last; i++)
+		{
+			signal.push_back;
+			complex a; dot b;
+
+			a.real += ampl * sin(2. * M_PI * samp_freq * i + samp_freq);
+			b.amplitude = a;
+			b.x_pos = i;
+			signal[i] = b;
+			size++;
+			sign_energy += a.real * a.real;
+		}
+	}
+
+	void SignalGen(int sin_count, double _ampl[], double _phase[], double _samp_freq[], int _first[], int _last[])
+	{
+
+		for (int i = 0; i < sin_count; i++)
+		{
+			SinGen(_ampl[i], _phase[i], _samp_freq[i], _first[i], _last[i]);
+		}
+
+
+	//	return signal;
+	}
+
+	void SignalwithWhiteNoise(vector<dot> k, int ampl)
+	{
+		for (int i = 0; i < size; i++)
+		{
+
+			double rand_part;
+			for (int j = 0; j < 45; j++)
+			{
+				rand_part += rand() % 1 - 1;
+			}
+			rand_part = rand_part / 45;
+			 
+			noise_energy += rand_part * rand_part;
+
+			k[i].amplitude.real = rand_part;
+			rand_part = 0;
+		}
+	}
+
+
+	void quicksort(vector<dot> item, int len)
+	{
+		qs(item, 0, len - 1);
+	}
+
+	void qs(vector<dot> item, int left, int right)
+	{
+		int i, j, x;
+		dot y;
+		i = left;   j = right;
+		x = item[(left + right) / 2].x_pos;
+		do {
+			while ((item[i].x_pos < x) && (i < right)); i++;
+			while ((x < item[j].x_pos) && (j > left)); j--;
+			if (i <= j)
+			{
+				y = item[i];
+				item[i] = item[j];
+				item[j] = y;
+				i++; j--;
+			}
+		} while (i <= j);
+
+		if (left < j) qs(item, left, j);
+		if (i < right) qs(item, i, right);
+	}
+};
