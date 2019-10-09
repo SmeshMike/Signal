8779d41624a2caf40a477444e6239cfe790b8e99
Elijah <Superil2q@yandex.ru>
2019-10-09 16:28:12 +0300

    rigth prop

--- SignalAnalisys/SignalAnalysis.h
+++ SignalAnalisys/SignalAnalysis.h
@@ -7,10 +7,11 @@
 
 using namespace std;
 
+typedef struct cmplx { float real; float image; } Cmplx;
+
 class AddNoise
 {
 public:
-
 	struct complex
 	{
 		double real = 0.0; double imagine = 0.0;
@@ -27,24 +28,24 @@ public:
 
 	double sign_energy = 0;
 
-	void quicksort(vector<dot> item, int len);
+	//void quicksort(vector<dot> item, int len);
 
-	void qs(vector<dot> item, int left, int right);
+	//void qs(vector<dot> item, int left, int right);
 
 	vector <dot> signal;			//Массив структуры для кранения значений амплитуд
 
-	void SinGen(double ampl, double phase, double samp_freq, int first, int last);		//Функция генящая синусоиду
+	//void SinGen(double ampl, double phase, double samp_freq, int first, int last);		//Функция генящая синусоиду
 
-	void SignalGen(int sin_count, double _ampl[], double _phase[], double _samp_freq[], int _first[], int _last[]);		//функция складывающая синусоиды
+	//void SignalGen(int sin_count, double _ampl[], double _phase[], double _samp_freq[], int _first[], int _last[]);		//функция складывающая синусоиды
 
-	void SignalwithWhiteNoise(vector<dot> k, int percent);
+	//void SignalwithWhiteNoise(vector<dot> k, int percent);
 
 	void SinGen(double ampl, double phase, double samp_freq, int first, int last)
 	{
 
 		for (int i = first; i < last; i++)
 		{
-			signal.push_back;
+			//signal.push_back();
 			complex a; dot b;
 
 			a.real += ampl * sin(2. * M_PI * samp_freq * i + samp_freq);
